a52a88baca1bf8d4481b5f85069f8ce86a70da0a
Chloe <chloebooton1@outlook.com>
2026-06-05 21:57:30 +0100

    Refactor login logic and improve session handling

--- login.php
+++ login.php
@@ -1,14 +1,26 @@
-<?php 
+<?php
+session_start();
 
-if (isset($_POST["submit"])) {
-    echo "it works";
+$conn = mysqli_connect("localhost", "root", "", "helpdesk");
 
-    $specialist_name = $_POST["specialist_name"];
-    $pwd = $_POST["pwd"];
+if (isset($_POST['submit'])) {
+
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+
+    $sql = "SELECT * FROM specialist WHERE username = ? AND pwd = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "ss", $username, $pwd);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_assoc($result);
+
+    if ($row) {
+        $_SESSION['username'] = $username;
+        header("Location: index.html"); 
+        exit();
+    } else {
+        echo "Incorrect username or password.";
     }
-
-else {
-    header("location: index.html");
 }
-
 ?>
